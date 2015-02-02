82f68f36ebcb6fbbb95eac972cae6e2524adef9d
Xintong Yu <yxt423@gmail.com>
2015-02-01 21:55:05 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -22,6 +22,9 @@
     
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
+    
+    // visualize physics bodies & joints
+    _physicsNode.debugDraw = TRUE;
 }
 
 // called on every touch in this scene
