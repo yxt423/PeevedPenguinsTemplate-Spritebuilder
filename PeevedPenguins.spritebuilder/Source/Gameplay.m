e21fa430a7d65a7b97e49510a4a2f879d872ed63
Xintong Yu <yxt423@gmail.com>
2015-02-01 22:31:38 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -56,20 +56,20 @@
     }
 }
 
-- (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
+- (void)touchMoved:(CCTouch *)touch withEvent:(UIEvent *)event
 {
     // whenever touches move, update the position of the mouseJointNode to the touch position
     CGPoint touchLocation = [touch locationInNode:_contentNode];
     _mouseJointNode.position = touchLocation;
 }
 
--(void) touchEnded:(UITouch *)touch withEvent:(UIEvent *)event
+-(void) touchEnded:(CCTouch *)touch withEvent:(UIEvent *)event
 {
     // when touches end, meaning the user releases their finger, release the catapult
     [self releaseCatapult];
 }
 
--(void) touchCancelled:(UITouch *)touch withEvent:(UIEvent *)event
+-(void) touchCancelled:(CCTouch *)touch withEvent:(UIEvent *)event
 {
     // when touches are cancelled, meaning the user drags their finger off the screen or onto something else, release the catapult
     [self releaseCatapult];
